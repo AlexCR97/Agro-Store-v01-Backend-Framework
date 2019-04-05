73802585a4e79a3909ea23e66d42d1c50dc475df
AlexCR97 <carp_97@outlook.com>
2019-04-04 21:26:28 -0600

    Initial commit

---
+++ src/CapaPresentacion/Main.java
@@ -0,0 +1,14 @@
+package CapaPresentacion;
+
+/*
+    Esta capa sera utilizada meramente para pruebas
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Agro Store v0.1");
+        System.out.println("Backend framework");
+    }
+
+}
