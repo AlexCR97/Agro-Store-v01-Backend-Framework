8c255a5aa5c784c4a49944f1ad9fec435b629602
gonzalez.martinez.16040@itsmante.edu.mx <tecm55993>
2019-04-10 13:58:57 -0500

    AGREGUE LA CONEXION A LA BASE DE DATOS AGROSTORE

--- src/CapaDatos/conexiones/BaseDeDatos.java
+++ src/CapaDatos/conexiones/BaseDeDatos.java
@@ -1,4 +1,43 @@
 package CapaDatos.conexiones;
 
-public class BaseDeDatos {
-}
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+    public class BaseDeDatos {
+
+        private Connection connection;
+
+        private String host;
+        private String dbName;
+        private String user;
+        private String password;
+
+        public BaseDeDatos(String host, String dbName, String user, String password) {
+            this.host = host;
+            this.dbName = dbName;
+            this.user = user;
+            this.password = password;
+        }
+
+        public Connection getConnection() {
+            if (connection != null)
+
+                return connection;
+
+            try {
+                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+                String url = String.format("jdbc:sqlserver://%s;databaseName=%s", host, dbName);
+
+                connection = DriverManager.getConnection(url, user, password);
+                System.out.println("conexion establecida");
+                return connection;
+            }
+            catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+
