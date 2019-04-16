257c0d850603c000f5e53458d8a2246ec9280f36
RobertHS13 <robertohs13000@gmail.com>
2019-04-15 22:09:01 -0500

    Agregue las primeras 5 entidades

---
+++ src/CapaEntidades/TipoUsuario.java
@@ -0,0 +1,43 @@
+package CapaEntidades;
+
+public class TipoUsuario {
+
+    private int idTipo;
+    private String tipoUsuario;
+
+    public TipoUsuario() {
+    }
+
+    public TipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public TipoUsuario(int idTipo, String tipoUsuario) {
+        this.idTipo = idTipo;
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public int getIdTipo() {
+        return idTipo;
+    }
+
+    public void setIdTipo(int idTipo) {
+        this.idTipo = idTipo;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    @Override
+    public String toString() {
+        return "TipoUsuario{" +
+                "idTipo=" + idTipo +
+                ", tipoUsuario='" + tipoUsuario + '\'' +
+                '}';
+    }
+}
